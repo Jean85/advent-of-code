6541876d82ea92647cb22fc9a972e2c60dde13cf
Alessandro Lai <alessandro.lai85@gmail.com>
2025-12-18 09:18:19 +0100

    Improve caching

--- src/Xmas2025/Day10/Machine.php
+++ src/Xmas2025/Day10/Machine.php
@@ -7,6 +7,8 @@ namespace Jean85\AdventOfCode\Xmas2025\Day10;
 class Machine
 {
     private array $cache = [];
+    private array $parityCache = [];
+    private array $buttonCache = [];
 
     private function __construct(
         public readonly IndicatorLights $indicatorLights,
@@ -84,17 +86,17 @@ class Machine
         $qty = 1;
         $foundValidPresses = [];
         $combinations = null;
-        if (isset($this->cache['parity'][$convertedJoltage->__toString()])) {
-            $combinations = $this->cache['parity'][$convertedJoltage->__toString()];
+        if (isset($this->parityCache[$convertedJoltage->__toString()])) {
+            $combinations = $this->parityCache[$convertedJoltage->__toString()];
             $qty = PHP_INT_MAX;
         } else {
-            $this->cache['parity'][$convertedJoltage->__toString()] = [];
+            $this->parityCache[$convertedJoltage->__toString()] = [];
         }
 
         do {
             foreach ($combinations ?? $this->getAllCombinationOfButtonsWithNoRepetitions($this->buttons, $qty) as $buttons) {
                 if ($convertedJoltage->canBeLightUpWith($buttons)) {
-                    $this->cache['parity'][$convertedJoltage->__toString()][] = $buttons;
+                    $this->parityCache[$convertedJoltage->__toString()][] = $buttons;
 
                     try {
                         $sub = $joltage->subtract($buttons);
@@ -135,9 +137,25 @@ class Machine
     /**
      * @param Button[] $possibleButtons
      *
+     * @return Button[]
+     */
+    private function getAllCombinationOfButtonsWithNoRepetitions(array $possibleButtons, int $qty): array
+    {
+        if ($qty > count($possibleButtons)) {
+            throw new \InvalidArgumentException('Not enough buttons');
+        }
+
+        return $this->buttonCache[$qty] ??= iterator_to_array(
+            $this->getAllCombinationOfButtonsWithNoRepetitionsRecursively($possibleButtons, $qty)
+        );
+    }
+
+    /**
+     * @param Button[] $possibleButtons
+     *
      * @return \Generator<Button[]>
      */
-    private function getAllCombinationOfButtonsWithNoRepetitions(array $possibleButtons, int $qty): \Generator
+    private function getAllCombinationOfButtonsWithNoRepetitionsRecursively(array $possibleButtons, int $qty): \Generator
     {
         if ($qty > count($possibleButtons)) {
             throw new \InvalidArgumentException('Not enough buttons');
@@ -151,7 +169,7 @@ class Machine
 
         for ($i = 0; $i <= count($possibleButtons) - $qty; ++$i) {
             $remainingButtons = array_slice($possibleButtons, $i);
-            foreach ($this->getAllCombinationOfButtonsWithNoRepetitions($remainingButtons, $qty - 1) as $followingButtons) {
+            foreach ($this->getAllCombinationOfButtonsWithNoRepetitionsRecursively($remainingButtons, $qty - 1) as $followingButtons) {
                 yield [$possibleButtons[$i], ...$followingButtons];
             }
         }
